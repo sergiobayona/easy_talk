197b9162ae450d48e8c5bab7893a0b207e3925e0
Sergio Bayona <sbayona@martianinteractive.com>
2024-04-08 11:34:36 -0500

    cleanup

--- lib/easy_talk/builders/composition_builder.rb
+++ lib/easy_talk/builders/composition_builder.rb
@@ -7,50 +7,52 @@ module EasyTalk
     class CompositionBuilder
       extend T::Sig
 
+      COMPOSER_TO_KEYWORD = {
+        'AllOfBuilder' => 'allOf',
+        'AnyOfBuilder' => 'anyOf',
+        'OneOfBuilder' => 'oneOf'
+      }.freeze
+
       # This class represents a builder for composing JSON schemas using the "allOf", "anyOf", or "oneOf" keywords.
-      class CompositionBuilder
-        sig { params(name: Symbol, type: T.untyped, _constraints: Hash).void }
-        def initialize(name, type, _constraints)
-          @composer_type = self.class.name.split('::').last
-          @name = name
-          @type = type
-          @context = {}
-        end
+      sig { params(name: Symbol, type: T.untyped, _constraints: Hash).void }
+      def initialize(name, type, _constraints)
+        @composer_type = self.class.name.split('::').last
+        @name = name
+        @type = type
+        @context = {}
+      end
 
-        # Builds the composed JSON schema.
-        #
-        # @return [void]
-        def build
-          @context[@name.to_sym] = {
-            type: 'object',
-            composer_keyword => schemas
-          }
-        end
+      # Builds the composed JSON schema.
+      #
+      # @return [void]
+      def build
+        @context[@name.to_sym] = {
+          type: 'object',
+          composer_keyword => schemas
+        }
+      end
 
-        private
+      # Returns the composer keyword based on the composer type.
+      #
+      # @return [String] The composer keyword.
+      def composer_keyword
+        COMPOSER_TO_KEYWORD[@composer_type]
+      end
 
-        # Returns the composer keyword based on the composer type.
-        #
-        # @return [String] The composer keyword.
-        def composer_keyword
-          COMPOSER_TO_KEYWORD[@composer_type]
+      # Returns an array of schemas for the composed JSON schema.
+      #
+      # @return [Array<Hash>] The array of schemas.
+      def schemas
+        items.map do |type|
+          type.respond_to?(:schema) ? type.schema : { type: type.to_s.downcase }
         end
+      end
 
-        # Returns an array of schemas for the composed JSON schema.
-        #
-        # @return [Array<Hash>] The array of schemas.
-        def schemas
-          items.map do |type|
-            type.respond_to?(:schema) ? type.schema : { type: type.to_s.downcase }
-          end
-        end
-
-        # Returns the items of the type.
-        #
-        # @return [T.untyped] The items of the type.
-        def items
-          @type.items
-        end
+      # Returns the items of the type.
+      #
+      # @return [T.untyped] The items of the type.
+      def items
+        @type.items
       end
     end
   end
