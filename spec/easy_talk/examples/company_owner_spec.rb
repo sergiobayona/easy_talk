1206e1d95b377549cc30994cd2ee6c5445ad11f6
Sergio Bayona <bayona.sergio@gmail.com>
2025-01-03 16:02:09 -0500

    custom type example

---
+++ spec/easy_talk/examples/company_owner_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'json for user model' do
+  let(:company) do
+    Class.new do
+      include EasyTalk::Model
+
+      def self.name
+        'Company'
+      end
+    end
+  end
+
+  let(:owner) do
+    Class.new do
+      include EasyTalk::Model
+
+      def self.name
+        'Owner'
+      end
+
+      define_schema do
+        property :first_name, String
+        property :last_name, String
+        property :dob, String
+      end
+    end
+  end
+
+  it 'enhances the schema using the provided block' do
+    stub_const('Owner', owner)
+    stub_const('Company', company)
+
+    Company.define_schema do
+      title 'Company'
+      property :name, String
+      property :owner, Owner
+    end
+
+
+    expected_schema = {
+      'type' => 'object',
+      'title' => 'Company',
+      'properties' => {
+        'name' => {
+          'type' => 'string'
+          },
+        'owner' => {
+          'type' => 'object',
+          'properties' => {
+            'first_name' => {
+              'type' => 'string'
+            },
+            'last_name' => {
+              'type' => 'string'
+            },
+            'dob' => {
+              'type' => 'string'
+            }
+          },
+          'required' => %w[first_name last_name dob]
+        }
+      },
+      'required' => %w[name owner]
+    }
+
+    expect(company.json_schema).to include_json(expected_schema)
+  end
+end
