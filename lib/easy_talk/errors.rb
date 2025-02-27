22ae17734426a7c504edce917e3779aea7ef7fc6
Sergio Bayona <bayona.sergio@gmail.com>
2025-02-27 14:54:59 -0500

    not used

--- lib/easy_talk/errors.rb
+++ lib/easy_talk/errors.rb
@@ -1,7 +1,6 @@
 module EasyTalk
   class Error < StandardError; end
   class ConstraintError < Error; end
-  class TypeMismatchError < Error; end
   class UnknownOptionError < Error; end
   class InvalidPropertyNameError < Error; end
 end
