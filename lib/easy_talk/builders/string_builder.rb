61c85c155160ab32d03d60898460eb6b8efa0eb4
Sergio Bayona <bayona.sergio@gmail.com>
2025-03-11 11:14:16 -0500

    default is already in the base builder

--- lib/easy_talk/builders/string_builder.rb
+++ lib/easy_talk/builders/string_builder.rb
@@ -15,8 +15,7 @@ module EasyTalk
         max_length: { type: Integer, key: :maxLength },
         enum: { type: T::Array[String], key: :enum },
         const: { type: String, key: :const },
-        default: { type: String, key: :default },
-        optional: { type: T::Boolean, key: :optional }
+        default: { type: String, key: :default }
       }.freeze
 
       sig { params(name: Symbol, constraints: Hash).void }
